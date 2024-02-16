946a826382b0b2191a98513a2e95bc3b4b6744a5
mako7200 <k168sa@gmail.com>
2024-02-16 23:45:49 +0900

    SNSっぽい画面を作成した！

---
+++ assets/js/top.js
@@ -0,0 +1,16 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const list = document.querySelectorAll('.list');
+
+    function activeLink() {
+        list.forEach(function(item) {
+            item.classList.remove('active');
+        });
+        this.classList.add('active');
+    }
+
+    list.forEach(function(item) {
+        item.addEventListener('click', activeLink);
+    });
+});
