779335a281c9559672c274dec31e39b4fd1188cd
mako7200 <k168sa@gmail.com>
2024-02-17 19:07:01 +0900

    時計をjsファイルに記述し直した。

---
+++ assets/js/clock.js
@@ -0,0 +1,25 @@
+/*===========================================================*/
+/*【】*/
+/*===========================================================*/
+
+
+
+/*===========================================================*/
+/*【時計】*/
+/*===========================================================*/
+document.addEventListener("DOMContentLoaded", function() {
+    let hr = document.querySelector('#hr');
+    let mn = document.querySelector('#mn');
+    let sc = document.querySelector('#sc');
+
+    setInterval(() => {
+        let day = new Date();
+        let hh = day.getHours() * 30;
+        let mm = day.getMinutes() * 6;
+        let ss = day.getSeconds() * 6;
+
+        hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+        mn.style.transform = `rotateZ(${mm}deg)`;
+        sc.style.transform = `rotateZ(${ss}deg)`;
+    }, 1000); // 1000ミリ秒ごとに実行
+});
